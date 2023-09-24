256d14b2407b3c22455ba0eb9aff98b9b6ddf051
TomHoang <126833810+thong2511@users.noreply.github.com>
2023-09-24 17:08:38 -0400

    Variables Name Change

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 
 #import the first dataset; seperated by tabs and doesn't have any columns names so this code addresses that 
 columns_names=['user_id','item_id','rating','timestamp']
-df=pd.read_csv("/Users/chandra/hack2023-umass/ml-100k/u.data", sep="\t",names=columns_names)        
+df=pd.read_csv(r"C:\Users\thong\Downloads\archive\ml-100k\u.data", sep="\t",names=columns_names)        
 
 #import the second dataset; similar to the first
 columns_names=['item_id','title','date', "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15", "A16", "A17", "A18", "A19", "A20", "A21"]
-movies = pd.read_csv('/Users/chandra/hack2023-umass/ml-100k/u.item', sep='|', encoding='latin-1', names=columns_names)
+movies = pd.read_csv(r'C:\Users\thong\Downloads\archive\ml-100k\u.item', sep='|', encoding='latin-1', names=columns_names)
 
 #we only need the first two columns to setup before we merge the two datasets together 
 movies=movies.iloc[: , 0:2]
@@ -35,16 +35,16 @@ We will recommend the most liked (leader) film to everyone.
 moviemat = df.pivot_table(index='user_id',columns='title',values='rating')
 moviemat.head()
 
-selection = input("input your movie name and year ex: 'Star Wars (1977)' ")
-starwars_user_ratings = moviemat[selection]
-starwars_user_ratings.head()
+selection = input(r"input your movie name and year ex: 'Star Wars (1977)' ")
+selection_user_ratings = moviemat[selection]
+selection_user_ratings.head()
 
-similar_to_starwars = moviemat.corrwith(starwars_user_ratings)
+similar_to_selection = moviemat.corrwith(selection_user_ratings)
 
 
-corr_starwars = pd.DataFrame(similar_to_starwars, columns=['Correlation'])
-corr_starwars.dropna(inplace=True)
-corr_starwars.sort_values('Correlation', ascending=False).head(10)
+corr_selection = pd.DataFrame(similar_to_selection, columns=['Correlation'])
+corr_selection.dropna(inplace=True)
+corr_selection.sort_values('Correlation', ascending=False).head(10)
 
 
 ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
@@ -54,8 +54,8 @@ print(ratings.head())
 
 print(ratings.sort_values('rating_vote_number',ascending=False).head())
 
-corr_starwars = corr_starwars.join(ratings['rating_vote_number'])
-print(corr_starwars.head())
+corr_selection = corr_selection.join(ratings['rating_vote_number'])
+print(corr_selection.head())
 
-final = corr_starwars[corr_starwars['rating_vote_number']>100].sort_values('Correlation',ascending=False).head()
+final = corr_selection[corr_selection['rating_vote_number']>100].sort_values('Correlation',ascending=False).head()
 print(final)
