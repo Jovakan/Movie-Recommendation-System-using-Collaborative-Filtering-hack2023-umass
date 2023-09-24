16310898ce3cdcb1a2da0243e0d4c6a81290f350
Jovakan <47262146+Jovakan@users.noreply.github.com>
2023-09-24 17:17:15 -0400

    changes to input

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 
 #import the first dataset; seperated by tabs and doesn't have any columns names so this code addresses that 
 columns_names=['user_id','item_id','rating','timestamp']
-df=pd.read_csv(r"C:\Users\thong\Downloads\archive\ml-100k\u.data", sep="\t",names=columns_names)        
+df=pd.read_csv("ml-100k/u.data", sep="\t",names=columns_names)        
 
 #import the second dataset; similar to the first
 columns_names=['item_id','title','date', "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15", "A16", "A17", "A18", "A19", "A20", "A21"]
-movies = pd.read_csv(r'C:\Users\thong\Downloads\archive\ml-100k\u.item', sep='|', encoding='latin-1', names=columns_names)
+movies = pd.read_csv('ml-100k/u.item', sep='|', encoding='latin-1', names=columns_names)
 
 #we only need the first two columns to setup before we merge the two datasets together 
 movies=movies.iloc[: , 0:2]
@@ -35,7 +35,7 @@ We will recommend the most liked (leader) film to everyone.
 moviemat = df.pivot_table(index='user_id',columns='title',values='rating')
 moviemat.head()
 
-selection = input(r"input your movie name and year ex: 'Star Wars (1977)' ")
+selection = input(r"input your movie name and year ex: 'Star Wars (1977)' "+"\n")
 selection_user_ratings = moviemat[selection]
 selection_user_ratings.head()
 
