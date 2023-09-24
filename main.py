b493e0742b56998f7d09fc7360a7672fa06b34fb
Jovakan <47262146+Jovakan@users.noreply.github.com>
2023-09-24 15:03:25 -0400

    changes that i made

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import json
 import pandas as pd
-from lightfm import LightFM
-from lightfm.evaluation import precision_at_k
+import os
 
-data_file = open("yelp_academic_dataset_checkin.json")
-data = []
-for line in data_file:
-    data.append(json.loads(line))
-checkin_df = pd.DataFrame(data)
-data_file.close()
 
+for dirname, _, filenames in os.walk('/Users/chandra/hack2023-umass/ml-100k'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+columns_names=['user_id','item_id','rating','timestamp']
+df=pd.read_csv("../input/movielens-100k-dataset/ml-100k/u.data", sep="\t",names=columns_names)        
+df.head(2)
 
 
 
