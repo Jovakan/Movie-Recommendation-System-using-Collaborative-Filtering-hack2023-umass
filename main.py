b3280e1af980dce9e51997d94ee07700e9166f64
Jovakan <47262146+Jovakan@users.noreply.github.com>
2023-09-24 14:21:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import json
 import pandas as pd
+from lightfm import LightFM
+from lightfm.evaluation import precision_at_k
+
 data_file = open("yelp_academic_dataset_checkin.json")
 data = []
 for line in data_file:
     data.append(json.loads(line))
 checkin_df = pd.DataFrame(data)
 data_file.close()
+
+
+
+
